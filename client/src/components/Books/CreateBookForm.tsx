b97d16582896f3f793cd5b71a9197d6954049459
kroniak <me@kroniak.net>
2024-06-19 23:33:47 +0300

    feat(forms) add formik

--- client/src/components/Books/CreateBookForm.tsx
+++ client/src/components/Books/CreateBookForm.tsx
@@ -1,83 +1,108 @@
-import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { isObjectEmpty, isStringEmpty } from '../../utils/utils';
+import { ErrorMessage, Field, Formik } from 'formik';
+import { isObjectEmpty } from '../../utils/utils';
 import { createBookActionCreator } from '../../actions/book-actions';
+import { Dispatch, DispatchWithoutAction } from 'react';
 
 const CreateBookForm = () => {
   const dispatch = useDispatch();
-  const [bookName, setBookName] = useState('');
-  const [bookAuthor, setBookAuthor] = useState('');
-  const [errors, setErrors] = useState<{ bookName?: string; bookAuthor?: string }>({});
 
-  const onSubmit = async () => {
-    let errors: { bookName?: string; bookAuthor?: string } = {};
-
-    if (isStringEmpty(bookName)) {
-      errors.bookName = 'required';
-    }
-    if (isStringEmpty(bookAuthor)) {
-      errors.bookAuthor = 'required';
-    }
-
-    if (isObjectEmpty(errors)) {
-      dispatch(
-        createBookActionCreator({
-          title: bookName,
-          author: bookAuthor,
-        }),
-      );
-      setBookName('');
-      setBookAuthor('');
-      setErrors({});
-    } else {
-      setErrors(errors);
-    }
+  const onSubmit = async (
+    values: any,
+    {
+      setSubmitting,
+      resetForm,
+    }: {
+      setSubmitting: Dispatch<boolean>;
+      resetForm: DispatchWithoutAction;
+    },
+  ) => {
+    // no need to validate; we disable the button if form not valid
+    dispatch(
+      createBookActionCreator({
+        title: values.bookName,
+        author: values.bookAuthor,
+      }),
+    );
+    resetForm();
+    setSubmitting(false);
   };
+
   return (
     <div className="create_book_form_wrapper">
-      <form
-        className="create_book_form row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          onSubmit();
-        }}
-      >
-        <div className="create_book_input col-md-6">
-          <label htmlFor="bookName" className="form-label">
-            Book Title
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="bookName"
-            value={bookName}
-            onChange={(e) => setBookName(e.target.value)}
-          />
-          {errors.bookName && <span className="form_error">This field is required</span>}
-        </div>
-        <div className="create_book_input col-md-6">
-          <label htmlFor="bookAuthor" className="form-label">
-            Book Author
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            id="bookAuthor"
-            value={bookAuthor}
-            onChange={(e) => setBookAuthor(e.target.value)}
-          />
-          {errors.bookAuthor && (
-            <span className="form_error">This field is required</span>
-          )}
-        </div>
-        <div className="create_book_form_add_btn_wrapper">
-          <button type="submit" className="btn btn-primary">
-            Create book
-          </button>
-        </div>
-      </form>
+      <Formik initialValues={{ bookName: '', bookAuthor: '' }} onSubmit={onSubmit}>
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+          /* and other goodies */
+        }) => (
+          <form className="create_book_form row" onSubmit={handleSubmit}>
+            <div className="create_book_input col-md-6">
+              <label htmlFor="bookName" className="form-label">
+                Book Title
+              </label>
+              <Field
+                type="text"
+                className="form-control"
+                name="bookName"
+                value={values.bookName}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                validate={validateRequiredField}
+                data-testid="bookName"
+              />
+              <ErrorMessage name="bookName">
+                {(message) => <span className="form_error">{message}</span>}
+              </ErrorMessage>
+            </div>
+            <div className="create_book_input col-md-6">
+              <label htmlFor="bookAuthor" className="form-label">
+                Book Author
+              </label>
+              <Field
+                type="text"
+                className="form-control"
+                name="bookAuthor"
+                value={values.bookAuthor}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                validate={validateRequiredField}
+                data-testid="bookAuthor"
+              />
+              <ErrorMessage name="bookAuthor">
+                {(message) => <span className="form_error">{message}</span>}
+              </ErrorMessage>
+            </div>
+            <div className="create_book_form_add_btn_wrapper">
+              <button
+                type="submit"
+                disabled={
+                  isObjectEmpty(touched) || !isObjectEmpty(errors) || isSubmitting
+                }
+                className="btn btn-primary"
+                data-testid="bookSubmit"
+              >
+                Create book
+              </button>
+            </div>
+          </form>
+        )}
+      </Formik>
     </div>
   );
 };
 
 export default CreateBookForm;
+
+function validateRequiredField(value: any) {
+  let error;
+  if (!value) {
+    error = 'This field is required';
+  }
+  return error;
+}
